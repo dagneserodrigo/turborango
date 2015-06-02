d2e51cae182c5858e4aa6aebcb252b0d3f3a8bf0
dagneserodrigo <dagnese.rodrigo@gmail.com>
2015-06-02 10:02:05 -0300

    add(Exercicio1)

---
+++ src/Solution/TurboRango.Dominio/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    internal enum Categoria
+    {
+        [Description("Comum")]
+        Comum,
+        [Description("Cozinha Natural")]
+        CozinhaNatural,
+        [Description("Cozinha Mexicana")]
+        CozinhaMexicana,
+        [Description("Churrascaria")]
+        Churrascaria,
+        [Description("Cozinha Japonesa")]
+        CozinhaJaponesa,
+        [Description("Fast Food")]
+        FastFood,
+        [Description("Pizzaria")]
+        Pizzaria
+    }
+}
