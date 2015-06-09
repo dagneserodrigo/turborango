395413eff5fea069ba2c028eb3bc92f06690f902
dagneserodrigo <dagnese.rodrigo@gmail.com>
2015-06-08 22:54:17 -0300

    add(TurboRango.Web)

--- src/Solution/TurboRango.Dominio/Restaurante.cs
+++ src/Solution/TurboRango.Dominio/Restaurante.cs
@@ -1,12 +1,12 @@
 ﻿
 namespace TurboRango.Dominio
 {
-    public class Restaurante
+    public class Restaurante : Entidade
     {
         public int? Capacidade { get; set; }
         public string Nome { get; set; }
         public Categoria Categoria { get; set; }
-        public Contato Contato { get; set; }
-        public Localizacao Localizacao { get; set; }
+        public virtual Contato Contato { get; set; }
+        public virtual Localizacao Localizacao { get; set; }
     }
 }
